433d8a5eb49c99c4cb664ce1cbc2416a04ae0ee0
MartenPostma <martenp@gmail.com>
2018-02-01 15:06:50 +0100

    v1 of official results

---
+++ official_results.py
@@ -0,0 +1,60 @@
+import os
+import utils
+
+# input arguments
+results_zip = '/Users/marten/Downloads/SemEval-2018 Task 5_ Counting Events and Participants in the Long Tail-17285-results.zip'
+results_folder = 'results'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+debug_value = 2
+
+# remove results folder if it exists
+utils.remove_folder(results_folder)
+
+# unzip
+team2folder = utils.unzip_it(results_zip, results_folder, debug=debug_value)
+
+# load data
+team2results = dict()
+for team, folder in team2folder.items():
+    path_scores_txt = os.path.join(folder, 'scores.txt')
+    team_results = utils.load_results(path_scores_txt)
+    team2results[team] = team_results
+
+assert len(team2results) == 6
+
+
+# to dfs
+subtask_and_metrics = [('Subtask 1', ['s1_doc_f1']),
+                       ('Subtask 2', ['s2_inc_accuracy', 's2_inc_rmse', 's2_doc_f1']),
+                       ('Subtask 3', ['s3_inc_accuracy', 's3_inc_rmse', 's3_doc_f1']),
+                       ('Event Coreference', ['s1_men_coref_avg'])
+                      ]
+
+
+
+caption_template = '\\caption{results for evaluation metric: \\textbf{%s}.\\hspace{\\textwidth} We mark explicitly with an asterisk the teams that had a task co-organizer as a team member}'
+
+with open('latex_input.txt', 'w') as outfile:
+    for subtask, target_metrics in subtask_and_metrics:
+
+        outfile.write('\\section{%s}\n' % subtask)
+        for target_metric in target_metrics:
+            result_df = utils.one_results_table(target_metric,
+                                                team2results,
+                                                debug=0)
+            latex_table = result_df.to_latex()
+            if latex_table:
+                outfile.write('\\begin{table}[H]\n')
+                outfile.write('\\centering\n')
+                outfile.write('\\captionsetup{justification=centering}')
+                latex_table = latex_table.replace('{}', 'Rank')
+                outfile.write(latex_table)
+
+                metric_name = result_df.columns[1].replace('_', '\\_')
+                outfile.write(caption_template % metric_name)
+                outfile.write('\\end{table}\n')
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,149 @@
+import shutil
+import os
+from glob import glob
+import zipfile
+import pandas
+from operator import itemgetter
+
+
+def remove_folder(folder):
+    """
+    remove folder if it exists
+
+    :param str folder: path to folder
+    """
+    if os.path.exists(folder):
+        shutil.rmtree(folder)
+
+
+def unzip_it(results_zip, results_folder, debug=False):
+    """
+    unzip results
+
+    :param debug:
+    :param str results_zip: path to download zip from codalab
+    :param str results_folder: path to results folder
+
+    :rtype: dict
+    :return: team_name -> results folder for that team
+    """
+    teamname2folder = dict()
+
+    with zipfile.ZipFile(results_zip, "r") as zip_ref:
+        zip_ref.extractall(results_folder)
+
+    submissions_dir = os.path.join(results_folder, 'submissions')
+    os.mkdir(submissions_dir)
+
+    for zip_folder in glob(results_folder + '/*output.zip'):
+
+        basename = os.path.basename(zip_folder)
+
+        team_name, dash, integer, *rest = basename.split()
+
+        if debug:
+            print()
+            print(team_name, dash, integer)
+
+        if all([team_name == 'paramitamirza',
+                integer == '7']):
+            team_name = 'IDDE'
+            if debug:
+                print('new team name', team_name)
+
+        teamfolder = os.path.join(submissions_dir, team_name)
+        with zipfile.ZipFile(zip_folder, "r") as zip_ref:
+            zip_ref.extractall(teamfolder)
+
+        teamname2folder[team_name] = teamfolder
+
+        if debug:
+            print('unpacked to', teamfolder)
+
+    return teamname2folder
+
+
+def load_results(path_to_scores_txt):
+    """
+
+    :param str path_to_scores_txt: path to scores txt
+
+    :rtype: dict
+    :return: mapping metric to output value of system wrt that metric
+    """
+    metric2value = dict()
+
+    with open(path_to_scores_txt) as infile:
+        for line in infile:
+            metric, value = line.strip().split(':')
+            metric2value[metric] = float(value)
+
+    assert len(metric2value) == 13, f'{metric2value} does not have need length of 13'
+
+    return metric2value
+
+
+def one_results_table(target_metric, team2results, debug=0):
+    """
+
+    :param team2results:
+    :param debug:
+    :param target_metric:
+    :return:
+    """
+    subtask = target_metric.split('_')[0]
+    key_perc_answered = f'{subtask}_answered'
+
+    list_of_lists = []
+    header_perc_answered = f'{subtask} % answered'
+    headers = ['team', target_metric]
+
+    if  target_metric.endswith('coref_avg'):
+        headers[1] = 'men_coref_avg'
+    else:
+        headers.append(header_perc_answered)
+
+    for team, results in team2results.items():
+
+        team_result_for_metric = results[target_metric]
+        perc_answered = results[key_perc_answered]
+
+        if team_result_for_metric == 0:
+            if debug >= 2:
+                print(f'ignored {target_metric} for {team}: {team_result_for_metric}')
+            continue
+
+        assert type(team) == str
+        assert type(team_result_for_metric) == float
+        assert team_result_for_metric != 0
+        assert type(perc_answered) == float
+
+        # add asterix to piek because he is an organizer
+        if team == 'Piek':
+            team = '*Piek'
+
+
+        one_row = [team, round(team_result_for_metric, 2)]
+
+        if not target_metric.endswith('coref_avg'):
+            one_row.append(round(perc_answered, 2))
+
+        list_of_lists.append(one_row)
+
+    if not list_of_lists:
+        if debug:
+            print(f'no results for at all {target_metric}')
+        return None
+
+    if target_metric.endswith('rmse'):
+        list_of_lists.sort(key=itemgetter(1), reverse=False)
+    else:
+        list_of_lists.sort(key=itemgetter(1), reverse=True)
+
+    if not target_metric.endswith('coref_avg'):
+        list_of_lists.sort(key=itemgetter(2), reverse=True)
+
+
+    metric_result_df = pandas.DataFrame(list_of_lists, columns=headers)
+    metric_result_df.index += 1
+    return metric_result_df
